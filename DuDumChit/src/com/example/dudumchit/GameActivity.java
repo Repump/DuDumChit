67151d7811ead50b25e5462b91664a8697685907
Repump <ojunhy0@gmail.com>
2014-05-08 03:14:54 +0900

    ResultActivity

--- DuDumChit/src/com/example/dudumchit/GameActivity.java
+++ DuDumChit/src/com/example/dudumchit/GameActivity.java
@@ -3,17 +3,26 @@ package com.example.dudumchit;
 import android.app.Activity;
 import android.content.Intent;
 import android.media.MediaPlayer;
-import android.media.MediaPlayer.TrackInfo;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
-public class GameActivity extends Activity {
+public class GameActivity extends Activity implements OnCompletionListener {
+	String path;
+	MediaPlayer player;
+	ProgressBar playBar;
+	int duration;
+	boolean playing;
+	Handler handler;
+
+	int score;
 	TextView scoreView;
 	ImageView characterView;
-	MediaPlayer player;
-	String path;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,33 +33,81 @@ public class GameActivity extends Activity {
 		path = i.getExtras().get("path").toString();
 		Log.i("PATH", path);
 		initialize();
-
-		player.start();
-		characterView.setRotationX(180);
-
+		musicStart();
 	}
 
 	private void initialize() {
-		scoreView = (TextView) findViewById(R.id.score);
-		scoreView.setText("0");
-		characterView = (ImageView) findViewById(R.id.character);
-
+		// Set MediaPlayer
 		player = new MediaPlayer();
-
 		try {
 			player.setDataSource(path);
 			player.prepare();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
 		player.setLooping(false);
+		player.setOnCompletionListener(this);
+
+		// Set Play Bar
+		playBar = (ProgressBar) findViewById(R.id.play_time);
+		duration = player.getDuration();
+		playBar.setMax(duration);
+		playBar.setProgress(0);
+		playing = true;
+		handler = new Handler() {
+			@Override
+			public void handleMessage(Message msg) {
+				super.handleMessage(msg);
+				if (playing) {
+					playBar.setProgress(player.getCurrentPosition());
+					sendEmptyMessageDelayed(0, 100);
+				}
+			}
+		};
+
+		// Set Score
+		score = 0;
+		scoreView = (TextView) findViewById(R.id.score);
+		scoreView.setText("0");
+
+		// Set Character
+		characterView = (ImageView) findViewById(R.id.character);
+		characterView.setImageResource(R.drawable.dudumchit);
+	}
+
+	@Override
+	public void onCompletion(MediaPlayer mp) {
+		musicStop();
+	}
+
+	private void musicStart() {
+		player.start();
+		handler.sendEmptyMessageDelayed(0, 100);
+	}
+
+	private void musicStop() {
+		playing = false;
+		handler.sendEmptyMessageAtTime(0, 0);
+		player.stop();
+		player.release();
+
+		// Send Score to ResultActivity
+		Intent i = new Intent(getApplicationContext(), ResultActivity.class);
+		i.putExtra("score", score);
+		startActivity(i);
+		finish();
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		if (playing)
+			musicStop();
 	}
 
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
-		player.stop(); //
-		player.release();
+		handler = null;
 	}
 }
--- DuDumChit/src/com/example/dudumchit/ResultActivity.java
+++ DuDumChit/src/com/example/dudumchit/ResultActivity.java
@@ -1,12 +1,31 @@
 package com.example.dudumchit;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ResultActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_result);
+
+		Intent i = getIntent();
+		int score = i.getExtras().getInt("score");
+
+		TextView result = (TextView) findViewById(R.id.show_score);
+		result.setText("당신의 점수는\n" + score + "점 입니다.");
+
+		Button okButton = (Button) findViewById(R.id.ok_button);
+		okButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
 	}
 }
